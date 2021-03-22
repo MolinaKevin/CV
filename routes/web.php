5135ec5229cc0477504fc16fe0d2a0697738da1d
Kevin Molina <molinakevinalejandro@gmail.com>
2021-03-22 21:59:28 +0100

    Empezando con la localizacion

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::domain('es.molinakev.in')->group(function () {
 
 });
 Route::get('/',function() {
-    dd($_SERVER,$_REQUEST,$_SESSION);
+    dd($_SERVER,$_REQUEST);
 })->name('index');
 
 //Route::get('/',Index::class)->name('index');
