b03258692af64fac3477a68ecd7035c06fbe208e
Kevin Molina <molinakevinalejandro@gmail.com>
2021-03-30 21:06:29 +0200

    Cambio box translatable

--- resources/views/curriculum/timeline.blade.php
+++ resources/views/curriculum/timeline.blade.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="md:flex-grow">
                         <h2 class="text-2xl font-medium text-paleta-secundario title-font mb-2">{!! $step->title !!}</h2>
-                        <p class="leading-relaxed text-paleta-secundario">{!! $step->getTranslation('description', \Session::get('locale')) !!}</p>
+                        <p class="leading-relaxed text-paleta-secundario">{!! $step->getTranslation('description', \Session::get('locale') ? \Session::get('locale') : \App::getLocale() ) !!}</p>
                         <a wire:click="$emit('timelineSelect',{{ $step->id }})" class="text-paleta-secundario inline-flex items-center mt-4 cursor-pointer">Leer mas
                             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M5 12h14"></path>
