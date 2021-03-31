9c2f380db493fe078ab997f430ea813741569b95
Kevin Molina <molinakevinalejandro@gmail.com>
2021-03-31 05:45:31 +0200

    Cambios todos translatable

--- app/Models/Screenshot.php
+++ app/Models/Screenshot.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Screenshot extends Model
 {
     use HasFactory;
+    use HasTranslations;
+
+    protected $translatable = ['title','subtitle','content'];
 
     protected  $fillable = ['path','title','subtitle','content','box_id'];
 
