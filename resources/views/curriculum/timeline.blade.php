dc922e588f19be08259e0c6883fb15018d6fbab3
Kevin Molina <molinakevinalejandro@gmail.com>
2021-03-31 12:04:38 +0200

    Cambios todos translatable

--- resources/views/curriculum/timeline.blade.php
+++ resources/views/curriculum/timeline.blade.php
@@ -10,7 +10,7 @@
                     <div class="md:flex-grow">
                         <h2 class="text-2xl font-medium text-paleta-secundario title-font mb-2">{!! $step->getTranslation('title', \Session::get('locale') ? \Session::get('locale') : \App::getLocale() ) !!}</h2>
                         <p class="leading-relaxed text-paleta-secundario">{!! $step->getTranslation('description', \Session::get('locale') ? \Session::get('locale') : \App::getLocale() ) !!}</p>
-                        <a wire:click="$emit('timelineSelect',{{ $step->id }})" class="text-paleta-secundario inline-flex items-center mt-4 cursor-pointer">__('Leer mas')
+                        <a wire:click="$emit('timelineSelect',{{ $step->id }})" class="text-paleta-secundario inline-flex items-center mt-4 cursor-pointer">{{ __('Leer mas') }}
                             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M5 12h14"></path>
                                 <path d="M12 5l7 7-7 7"></path>
