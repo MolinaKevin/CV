c74a6c4935df4778046406c1da0f37c1b003c471
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-06 12:39:23 +0200

    Agregando cambio de Idioma

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -20,6 +20,11 @@
             {{ __('Linea de Tiempo') }}
         </x-nav-link>
     </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
     <!-- Hamburger -->
     <div class="-mr-2 flex items-center sm:hidden">
         <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
