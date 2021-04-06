6ec45736647f368300053704beffdb176ea11819
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-06 12:50:20 +0200

    Agregando cambio de Idioma

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -20,15 +20,11 @@
             {{ __('Linea de Tiempo') }}
         </x-nav-link>
     </nav>
-    <a href="#" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">ES
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
+    <a href="#" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 mr-2 md:mt-0">
+        ES
     </a>
-    <a href="#" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">DE
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
+    <a href="#" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+        DE
     </a>
     <!-- Hamburger -->
     <div class="-mr-2 flex items-center sm:hidden">
