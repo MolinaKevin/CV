b7db4c80886ecbc11049c11f81f624984b78d667
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-07 22:54:56 +0200

    Modal en inicio

--- resources/views/curriculum/index.blade.php
+++ resources/views/curriculum/index.blade.php
@@ -62,10 +62,10 @@
         </x-slot>
 
         <x-slot name="content">
-            <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-<textarea id="message" name="message" class="lg:w-2/3 rounded-lg md:w-1/2 sm:mr-10 p-10 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            <div class="container px-5 py-12 mx-auto flex sm:flex-nowrap flex-wrap">
+<textarea id="message" name="message" placeholder="{{ __('Escribeme a mi directamente') }}" class="lg:w-2/3 rounded-lg md:w-1/2 sm:mr-10 p-10 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
 
-                <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                <div class="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                   <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
                   <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
                   <div class="relative mb-4">
