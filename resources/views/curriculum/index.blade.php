64f3d09bcaad5eacf5606aed69ff40b931564cb0
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-07 20:06:00 +0200

    Modal en inicio

--- resources/views/curriculum/index.blade.php
+++ resources/views/curriculum/index.blade.php
@@ -20,7 +20,7 @@
 			<p class="mt-4 text-sm text-paleta-secundario max-h-40 overflow-y-auto">{{ __('Si estás acá probablemente estés pensando en contratarme y por ende me veo en la necesidad de advertirte un par de cuestiones. Me interesa dar todo. Si estás buscando una mano de obra robotizada y sin criterio no soy la persona indicada. Si estás buscando alguien que se sepa todos los conceptos teóricos de memoria tampoco soy ese tipo de profesional. Yo soy una persona comprometida, la cual se ha embarcado en diferentes travesías y que se esfuerza por encontrar las mejores soluciones a los problemas que se le presenten, tanto en la profesión como en el resto de la vida. Soy apasionado y leal, me gusta dar todo lo que tengo. Me gusta aprender y no le tengo miedo a ninguna tecnología. A lo largo de mi carrera me he topado con problemas tan heterogéneos que conozco muchos caminos y posibles soluciones, también por ello me gusta compartir mi experiencia y no me gusta que las cosas se resuelvan de sin un objetivo o fin claro. Si solo se me va a imponer trabajo no me contactes por favor, no pierdas tu tiempo, no trabajo bien desmotivado y el hecho de no ser escuchado es lo que más me desmotiva. Y si no estás seguro, no dudes en contactarme con las herramientas de acá abajo y podemos charlarlo en persona, soy un tipo afable :).') }}</p>
 
 			<div class="pt-4 pb-8">
-				<button wire:click="showModal" class="bg-paleta-secundario hover:bg-paleta-terciario text-paleta-quinario font-bold py-2 px-4 rounded-full">
+				<button wire:model="showModal" class="bg-paleta-secundario hover:bg-paleta-terciario text-paleta-quinario font-bold py-2 px-4 rounded-full">
                     {{ __('Enviame un mail') }}
 				</button>
 			</div>
@@ -59,14 +59,72 @@
 
     <x-dialog-modal wire:model="modal">
         <x-slot name="title">
-            {{ __('Resultados') }}
+            {{ __('Enviame un mail') }}
         </x-slot>
 
         <x-slot name="content">
-            <div class="max-w-2xl mx-auto bg-white dark:bg-gray-800 overflow-hidden shadow-md rounded-lg">
-                <img class="w-full h-100 object-cover" src="{{ asset('storage/images/yo.jfif') }}" alt="Article">
-
-            </div>
+            <section class="text-gray-600 body-font relative">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+    </div>
+    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+      <div class="flex flex-wrap -m-2">
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+        </div>
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+            <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+        </div>
+        <div class="p-2 w-full">
+          <div class="relative">
+            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+            <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+          </div>
+        </div>
+        <div class="p-2 w-full">
+          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        </div>
+        <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+          <a class="text-indigo-500">example@email.com</a>
+          <p class="leading-normal my-5">49 Smith St.
+            <br>Saint Cloud, MN 56301
+          </p>
+          <span class="inline-flex">
+            <a class="text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a class="ml-4 text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a class="ml-4 text-gray-500">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+            <a class="ml-4 text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+              </svg>
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
         </x-slot>
 
         <x-slot name="footer">
