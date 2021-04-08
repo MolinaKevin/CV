100b82a5acff00e3c6d97a2aed1e640acbc5df18
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-08 22:02:52 +0200

    Colores Sophie

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'mr-5 text-paleta-primario px-1 pt-1 border-b-2 border-transparent hover:text-paleta-primario hover:border-b-2 hover:border-paleta-secundario leading-5 focus:outline-none focus:border-b-2 focus:border-paleta-secundario transition duration-150 ease-in-out'
+            ? 'mr-5 text-paleta-secundario px-1 pt-1 border-b-2 border-transparent hover:text-paleta-primario hover:border-b-2 hover:border-paleta-secundario leading-5 focus:outline-none focus:border-b-2 focus:border-paleta-secundario transition duration-150 ease-in-out'
             : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-paleta-primario hover:border-b-2 hover:text-theme-hover hover:border-theme-hover focus:outline-none focus:text-theme-hover focus:border-theme-hover transition duration-150 ease-in-out';
 @endphp
 
