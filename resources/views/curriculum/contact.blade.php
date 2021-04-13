b101bf986b7ad1873075e8e617ec9abcac5a1333
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-13 13:44:13 +0200

    Contact

--- resources/views/curriculum/contact.blade.php
+++ resources/views/curriculum/contact.blade.php
@@ -31,19 +31,6 @@
                     </svg>
                     Save</div>
             </button>
-            <button class="text-base  rounded-l-none rounded-r-none border-l-0 border-r-0  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
-        hover:bg-teal-700 hover:text-teal-100
-        bg-teal-100
-        text-teal-700
-        border duration-200 ease-in-out
-        border-teal-600 transition">
-                <div class="flex leading-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit w-5 h-5 mr-1">
-                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                    </svg>
-                    Edit</div>
-            </button>
             <button class="text-base  rounded-l-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
         hover:bg-teal-700 hover:text-teal-100
         bg-teal-100
