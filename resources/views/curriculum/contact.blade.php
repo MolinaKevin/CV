004e8ce6937dee3f68177760e7307ab8993b26cf
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-13 13:03:18 +0200

    Contact

--- resources/views/curriculum/contact.blade.php
+++ resources/views/curriculum/contact.blade.php
@@ -18,31 +18,47 @@
             <div class="relative mb-4">
 <div class="flex m-2">
             <button class="text-base  rounded-r-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
-        hover:bg-gray-200
-        bg-gray-100
-        text-gray-700
-        border duration-200 ease-in-out
-        border-gray-600 transition">
-                <div class="flex leading-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left w-5 h-5">
-                        <polyline points="15 18 9 12 15 6"></polyline>
-                    </svg>
-                    Back</div>
-            </button>
-            <button class="text-base  rounded-l-none border-l-0  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
-        hover:bg-teal-200
+        hover:bg-teal-700 hover:text-teal-100
         bg-teal-100
         text-teal-700
         border duration-200 ease-in-out
         border-teal-600 transition">
-                <div class="flex leading-5">Next
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right w-5 h-5 ml-1">
-                        <polyline points="9 18 15 12 9 6"></polyline>
+                <div class="flex leading-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-save w-5 h-5 mr-1">
+                        <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
+                        <polyline points="17 21 17 13 7 13 7 21"></polyline>
+                        <polyline points="7 3 7 8 15 8"></polyline>
                     </svg>
-                </div>
+                    Save</div>
+            </button>
+            <button class="text-base  rounded-l-none rounded-r-none border-l-0 border-r-0  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
+        hover:bg-teal-700 hover:text-teal-100
+        bg-teal-100
+        text-teal-700
+        border duration-200 ease-in-out
+        border-teal-600 transition">
+                <div class="flex leading-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit w-5 h-5 mr-1">
+                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                    </svg>
+                    Edit</div>
+            </button>
+            <button class="text-base  rounded-l-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
+        hover:bg-teal-700 hover:text-teal-100
+        bg-teal-100
+        text-teal-700
+        border duration-200 ease-in-out
+        border-teal-600 transition">
+                <div class="flex leading-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye w-5 h-5 mr-1">
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                        <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                    View</div>
             </button>
         </div>
-            </div>
+           </div>
             <div class="relative mb-4">
                 <label for="email" class="leading-7 text-sm text-paleta-secundario">{{ __('Email') }}</label>
                 <input type="email" id="email" name="email" class="w-full bg-white rounded border-2 border-paleta-cuaternario focus:border-paleta-cuaternario focus:ring-2 focus:ring-paleta-cuaternario text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
