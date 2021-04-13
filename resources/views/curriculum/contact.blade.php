e4e200a5fade5a3eabb7d38eded22fd0d6f9da4f
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-13 12:43:53 +0200

    Contact

--- resources/views/curriculum/contact.blade.php
+++ resources/views/curriculum/contact.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="container w-full sm:py-10 px-8 mx-auto flex">
         <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">{{ __('') }}</h2>
+            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">{{ __('Formulario de contacto') }}</h2>
             <div class="relative mb-4">
                 <label for="email" class="leading-7 text-sm text-paleta-secundario">{{ __('Email') }}</label>
                 <input type="email" id="email" name="email" class="w-full bg-white rounded border-2 border-paleta-cuaternario focus:border-paleta-cuaternario focus:ring-2 focus:ring-paleta-cuaternario text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
