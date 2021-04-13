bb5ee9f00b7e697fd96dce1d04ea8bf7c6475cfa
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-13 12:51:25 +0200

    Contact

--- resources/views/curriculum/contact.blade.php
+++ resources/views/curriculum/contact.blade.php
@@ -6,6 +6,12 @@
         <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">{{ __('Formulario de contacto') }}</h2>
             <div class="relative mb-4">
+                <button class="group bg-yellow-500 focus:bg-yellow-600">
+                    <svg class="text-white group-focus:text-yellow-300"></svg>
+                    Bookmark
+                </button>
+            </div>
+            <div class="relative mb-4">
                 <label for="email" class="leading-7 text-sm text-paleta-secundario">{{ __('Email') }}</label>
                 <input type="email" id="email" name="email" class="w-full bg-white rounded border-2 border-paleta-cuaternario focus:border-paleta-cuaternario focus:ring-2 focus:ring-paleta-cuaternario text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
