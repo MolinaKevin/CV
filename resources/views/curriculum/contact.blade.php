95b85e5d603c0a94baf44cc71b52bc66b7872cde
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-14 09:11:00 +0200

    Contact

--- resources/views/curriculum/contact.blade.php
+++ resources/views/curriculum/contact.blade.php
@@ -25,9 +25,7 @@
         border-teal-600 transition">
                 <div class="flex leading-5">
                     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
-                        <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
-                        <polyline points="17 21 17 13 7 13 7 21"></polyline>
-                        <polyline points="7 3 7 8 15 8"></polyline>
+                        <circle cx="12" cy="12" r="10"></circle>
                     </svg>
                     Save</div>
             </button>
@@ -38,7 +36,11 @@
         border duration-200 ease-in-out
         border-teal-600 transition">
                 <div class="flex leading-5">
-                    <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1"><polyline points="20 6 9 17 4 12"></polyline></svg>
+                    <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
+                        <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                        <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                   </svg>
+
                     View</div>
             </button>
         </div>
