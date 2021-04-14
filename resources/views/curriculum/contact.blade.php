f62abe5bb5df635fa005bbdf6ab0ba652fc98e39
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-14 10:41:09 +0200

    Contact

--- resources/views/curriculum/contact.blade.php
+++ resources/views/curriculum/contact.blade.php
@@ -23,8 +23,8 @@
 
                     wire:click="setEmail(0)">
                         <div class="flex leading-5">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
-                             @if($active == 1)
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather w-5 h-5 mr-1">
+                             @if($active == 0)
                                 <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
                                 <polyline points="22 4 12 14.01 9 11.01"></polyline>
                             @else
@@ -48,7 +48,7 @@
 
                     wire:click="setEmail(1)">
                         <div class="flex leading-5">
-                            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
+                            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather w-5 h-5 mr-1">
 
                             @if($active == 1)
                                 <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
