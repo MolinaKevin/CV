c949a9308aa581932672dabc193ecea83d9b90ea
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-14 18:53:54 +0200

    Contact

--- resources/views/mails/test.blade.php
+++ resources/views/mails/test.blade.php
@@ -1,5 +1,5 @@
 {{ __('Hola') }} <i>{{ $email->sender }}</i>,
-use _;
+<br/>
 <p>{{ __('Muchas gracias por ponerte en contacto conmigo. Recibirá una respuesta de mi parte a la brevedad.') }}</p>
 
 <p>{{ __('Aquí puede ver el contenido del mensaje que me ha enviado:') }}</p>
@@ -7,10 +7,12 @@ use _;
 <div>
 {{ $email->message }}
 </div>
-
+<br/>
 <p>{{ __('Si usted no se ha puesto en contacto conmigo, tenga en cuenta que alguien pudiere estar usando sus credenciales.') }}</p>
 
 {{ __('Muchas gracias') }}
 <br/>
-<a href="https://molinakev.in">https://molinakev.in</a>
+<br/>
 <i>Kevin Molina</i>
+<br/>
+<a href="https://molinakev.in">https://molinakev.in</a>
