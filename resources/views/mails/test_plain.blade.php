a4e9b3cd80be8b1f06a89b7138674ffc4af336ea
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-14 18:44:57 +0200

    Contact

--- resources/views/mails/test_plain.blade.php
+++ resources/views/mails/test_plain.blade.php
@@ -3,11 +3,7 @@ This is a demo email for testing purposes! Also, it's the HTML version.
 
 Demo object values:
 
-Demo One: {{ $email->demo_one }}
-Demo Two: {{ $email->demo_two }}
-
 Values passed by With method:
 
 
 Thank You,
-{{ $email->sender }}
