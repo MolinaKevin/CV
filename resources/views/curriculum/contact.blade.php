40c43c1629dc4a0a869f74ec5ef422d3b4adc4cf
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-14 09:13:01 +0200

    Contact

--- resources/views/curriculum/contact.blade.php
+++ resources/views/curriculum/contact.blade.php
@@ -6,45 +6,35 @@
         <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">{{ __('Formulario de contacto') }}</h2>
             <div class="relative mb-4">
-                <button type="button" class="group inline-flex items-center py-2 px-4 bg-paleta-secundario text-white font-semibold rounded-lg shadow-md focus:bg-amber-600 focus:outline-none">
-                    <svg fill="currentColor" viewBox="0 0 20 20" class="-ml-1 mr-3 w-5 h-5 text-white group-focus:text-amber-300"><path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z"></path></svg>
-                    Bookmark
-                </button>
-                <button type="button" class="group inline-flex items-center py-2 px-4 bg-paleta-secundario text-white font-semibold rounded-lg shadow-md focus:bg-amber-600 focus:outline-none">
-                    <svg fill="currentColor" viewBox="0 0 20 20" class="-ml-1 mr-3 w-5 h-5 text-white group-focus:text-amber-300"><path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z"></path></svg>
-                    Bookmark
-                </button>
-            </div>
-            <div class="relative mb-4">
-<div class="flex m-2">
-            <button class="text-base  rounded-r-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
-        hover:bg-teal-700 hover:text-teal-100
-        bg-teal-100
-        text-teal-700
-        border duration-200 ease-in-out
-        border-teal-600 transition">
-                <div class="flex leading-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
-                        <circle cx="12" cy="12" r="10"></circle>
-                    </svg>
-                    Save</div>
-            </button>
-            <button class="text-base  rounded-l-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
-        hover:bg-teal-700 hover:text-teal-100
-        bg-teal-100
-        text-teal-700
-        border duration-200 ease-in-out
-        border-teal-600 transition">
-                <div class="flex leading-5">
-                    <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
-                        <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-                        <polyline points="22 4 12 14.01 9 11.01"></polyline>
-                   </svg>
+                <div class="flex m-2 ">
+                    <button class="text-base  rounded-r-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
+                hover:bg-teal-700 hover:text-teal-100
+                bg-teal-100
+                text-teal-700
+                border duration-200 ease-in-out
+                border-teal-600 transition">
+                        <div class="flex leading-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
+                                <circle cx="12" cy="12" r="10"></circle>
+                            </svg>
+                            Save</div>
+                    </button>
+                    <button class="text-base  rounded-l-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer
+                hover:bg-teal-700 hover:text-teal-100
+                bg-teal-100
+                text-teal-700
+                border duration-200 ease-in-out
+                border-teal-600 transition">
+                        <div class="flex leading-5">
+                            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle w-5 h-5 mr-1">
+                                <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                                <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                           </svg>
 
-                    View</div>
-            </button>
-        </div>
-           </div>
+                            View</div>
+                    </button>
+                </div>
+            </div>
             <div class="relative mb-4">
                 <label for="email" class="leading-7 text-sm text-paleta-secundario">{{ __('Email') }}</label>
                 <input type="email" id="email" name="email" class="w-full bg-white rounded border-2 border-paleta-cuaternario focus:border-paleta-cuaternario focus:ring-2 focus:ring-paleta-cuaternario text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
