c89e5f376f36e3306279042779d9cc4297c3e905
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-18 21:39:04 +0200

    Responsive

--- resources/views/curriculum/timeline.blade.php
+++ resources/views/curriculum/timeline.blade.php
@@ -33,7 +33,7 @@
         </x-slot>
 
         <x-slot name="content">
-		    <img src="{{ asset('storage/images/' . $screen->path) }}" class="rounded-none shadow-2xl hidden lg:block max-h-16">
+		    <img src="{{ asset('storage/images/' . $screen->path) }}" class="rounded-none shadow-2xl hidden lg:block max-h-96">
         </x-slot>
 
         <x-slot name="footer">
