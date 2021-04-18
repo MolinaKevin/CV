1e889ca781b4a14fb4cce2b8478ee38d7ea67b6b
Kevin Molina <molinakevinalejandro@gmail.com>
2021-04-18 21:01:53 +0200

    Pop-up screens

--- resources/views/components/image-modal.blade.php
+++ resources/views/components/image-modal.blade.php
@@ -1,7 +1,7 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div class="px-6 py-4">
+    <div class="">
         <div class="text-lg">
             {{ $title }}
         </div>
